bfb52ea3acfdc67379a26cc24363386499a3c447
davidavila451 <avila_david_2@yahoo.com>
2020-03-28 08:35:24 -0500

    Main and the Login fxml

--- src/application/Main/Main.java
+++ src/application/Main/Main.java
@@ -1,5 +1,33 @@
 package application.Main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+/**
+ * This class is used to load up the application when the app is first run.
+ * After starting the app it will then jump to the login controller.
+ * @author David
+ *
+ */
+
+public class Main extends Application{
+	public static Stage stage;
+	@Override
+	public void start(Stage primaryStage) {
+		stage = primaryStage;
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+			primaryStage.setScene(new Scene(root, 500, 500));
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
